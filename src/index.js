79b2e9b7083d420641025512d127eb51b717b633
shaun <shaun.groenewald@gmail.com>
2020-02-24 18:48:07 +0200

    progress

--- src/index.js
+++ src/index.js
@@ -10,33 +10,96 @@ import {
         ID
 */
 
+const users = [{
+        id: '1',
+        name: 'Shaun',
+        email: 's@g.com',
+        age: 29
+    },
+    {
+        id: '2',
+        name: 'Shaun2',
+        email: 's@g.com2',
+        age: 29
+    },
+    {
+        id: '3',
+        name: 'Piet',
+        email: 's@g.com3'
+    }
+]
+
+const posts = [{
+        id: '1',
+        title: 'Post 1',
+        body: 'test',
+        published: true
+    },
+    {
+        id: '2',
+        title: 'Post 2',
+        body: 'lalala',
+        published: true
+    },
+    {
+        id: '3',
+        title: 'Post 3',
+        body: 'apex yass',
+        published: false
+    }
+]
 // Type defs (schema)
 const typeDefs = `
     type Query {
+        me: User!
+        post: Post!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
         title: String!
-        price: Float!
-        releaseYear: Int
-        rating: Float
-        inStock: Boolean
+        body: String!
+        published: Boolean!
     }
 `;
 // Resolvers
 const resolvers = {
     Query: {
-        title() {
-            return "Item title"
+        users(parent, args, ctx, info) {
+            if (args.query) {
+                return users.filter(elem => elem.name.toLowerCase().includes(args.query.toLowerCase()));
+            } else
+                return users;
         },
-        releaseYear() {
-            return 2019
+        posts(parent, args, ctx, info) {
+            if (args.query) {
+                return posts.filter(elem => elem.title.toLowerCase().includes(args.query.toLowerCase()) || elem.body.toLowerCase().includes(args.query.toLowerCase()));
+            } else
+                return posts;
         },
-        price() {
-            return 29;
+        me() {
+            return {
+                id: 1,
+                name: "Shaun",
+                email: "s@g.com"
+            }
         },
-        rating() {
-            return null;
-        },
-        inStock() {
-            return false
+        post() {
+            return {
+                id: 1,
+                title: "Test T",
+                body: "Test B",
+                published: true
+            }
         },
     }
 }
